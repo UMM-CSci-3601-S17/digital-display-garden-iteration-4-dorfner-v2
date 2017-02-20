41aea822d7a7f4a35a49607ba7bc8b190c1c39fb
Nic McPhee <mcphee@morris.umn.edu>
2017-02-19 23:08:29 -0600

    Added a bunch of Mongo examples

--- server/src/main/java/umm3601/mongotest/Mongotest.java
+++ server/src/main/java/umm3601/mongotest/Mongotest.java
@@ -6,22 +6,29 @@ package umm3601.mongotest;
  *
  */
 
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.MongoException;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
 
-import com.mongodb.ServerAddress;
-import java.util.ArrayList;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
 
 public class Mongotest {
 
     public void connectToMongo() {
-        try {
+        Block<Document> printBlock = new Block<Document>() {
+            @Override
+            public void apply(final Document document) {
+                System.out.println(document.toJson());
+            }
+        };
 
-            //// Set up our authentication credentials
-            //// Thanks to: http://stackoverflow.com/a/21860551
+        //// Set up our authentication credentials
+        //// Thanks to: http://stackoverflow.com/a/21860551
             /*
             List<MongoCredential> credentials = new ArrayList<MongoCredentials>();
             credentials.add(
@@ -34,20 +41,45 @@ public class Mongotest {
             );
             */
 
-            // Set up our server address
-            // (Default host: 'localhost', default port: 27017)
-            ServerAddress testAddress = new ServerAddress();
+        // Set up our server address
+        // (Default host: 'localhost', default port: 27017)
+        ServerAddress testAddress = new ServerAddress();
 
-            // Try connecting to the server
-            //MongoClient mongoClient = new MongoClient(testAddress, credentials);
-            MongoClient mongoClient = new MongoClient(); // Defaults!
-            System.out.println("Connection to database successful");
+        // Try connecting to the server
+        //MongoClient mongoClient = new MongoClient(testAddress, credentials);
+        MongoClient mongoClient = new MongoClient(); // Defaults!
 
-            // Try connecting to a database
-            MongoDatabase db = mongoClient.getDatabase("testdb");
+        // Try connecting to a database
+        MongoDatabase db = mongoClient.getDatabase("testdb");
+        System.out.println("Connection to database successful");
 
-        }catch(Exception e){
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-        }
+        MongoCollection<Document> documents = db.getCollection("users");
+
+        System.out.println("EVERYONE");
+        documents.find().forEach(printBlock);
+
+        System.out.println("\nJUST CHRIS");
+        documents.find(eq("name", "Chris")).forEach(printBlock);
+
+        System.out.println("\nOVER 25");
+        documents.find(gt("age", 25)).forEach(printBlock);
+
+        System.out.println("\nOVER 25, SORTED BY NAME");
+        documents.find(gt("age", 25)).sort(Sorts.ascending("name")).forEach(printBlock);
+
+        System.out.println("\nOVER 25 and IBMers");
+        documents.find(and(gt("age", 25), eq("company", "IBM"))).forEach(printBlock);
+
+        System.out.println("\nJUST NAME AND EMAIL");
+        documents.find().projection(fields(include("name", "email"))).forEach(printBlock);
+
+        System.out.println("\nJUST NAME AND EMAIL, NO IDs");
+        documents.find().projection(fields(include("name", "email"), excludeId())).forEach(printBlock);
+
+        System.out.println("\nJUST NAME AND EMAIL, NO IDs, SORTED BY COMPANY");
+        documents.find()
+                .sort(Sorts.ascending("company"))
+                .projection(fields(include("name", "email"), excludeId()))
+                .forEach(printBlock);
     }
 }
