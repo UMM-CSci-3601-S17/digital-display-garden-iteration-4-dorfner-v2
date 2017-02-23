e11391d96ed8fea7d6d6a1fa371c895648306af1
Nic McPhee <mcphee@morris.umn.edu>
2017-02-23 09:18:10 -0600

    Continued work on UserController tests
    I've got the `find` tests working, but the test where we search by ID isn't happy because I'm having trouble
    figuring out how to set an ID when I create an object.

--- server/src/test/java/umm3601/user/UserControllerSpec.java
+++ server/src/test/java/umm3601/user/UserControllerSpec.java
@@ -3,11 +3,13 @@ package umm3601.user;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
 import org.bson.*;
 import org.bson.codecs.*;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.json.JsonReader;
+import org.bson.types.ObjectId;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,6 +27,7 @@ import static org.junit.Assert.assertEquals;
 public class UserControllerSpec
 {
     private UserController userController;
+    private String samsIdString;
 
     @Before
     public void clearAndPopulateDB() throws IOException {
@@ -51,12 +54,19 @@ public class UserControllerSpec
                 "                    company: \"Frogs, Inc.\",\n" +
                 "                    email: \"jamie@frogs.com\"\n" +
                 "                }"));
-        testUsers.add(Document.parse("{\n" +
+        Document sam = Document.parse("{\n" +
                 "                    name: \"Sam\",\n" +
                 "                    age: 45,\n" +
                 "                    company: \"Frogs, Inc.\",\n" +
                 "                    email: \"sam@frogs.com\"\n" +
-                "                }"));
+                "                }");
+        ObjectId samsId = new ObjectId();
+        samsIdString = samsId.toHexString();
+        System.out.println(samsIdString);
+        sam.put("_id", JSON.serialize(samsId));
+        System.out.println(sam);
+        testUsers.add(sam);
+        System.out.println(testUsers);
         userDocuments.insertMany(testUsers);
 
         // It might be important to construct this _after_ the DB is set up
@@ -85,7 +95,7 @@ public class UserControllerSpec
     }
 
     @Test
-    public void listAllUsers() {
+    public void getAllUsers() {
         Map<String, String[]> emptyMap = new HashMap<>();
         String jsonResult = userController.listUsers(emptyMap);
         System.out.println(jsonResult);
@@ -100,5 +110,34 @@ public class UserControllerSpec
                 .collect(Collectors.toList());
         List<String> expectedNames = Arrays.asList("Chris", "Jamie", "Pat", "Sam");
         assertEquals("Names should match", expectedNames, names);
+    }
+
+    @Test
+    public void getUsersWhoAre37() {
+        Map<String, String[]> argMap = new HashMap<>();
+        argMap.put("age", new String[] { "37" });
+        String jsonResult = userController.listUsers(argMap);
+        System.out.println(jsonResult);
+        BsonArray docs = parseJsonArray(jsonResult);
+        System.out.println(docs);
+
+        assertEquals("Should be 2 users", 2, docs.size());
+        List<String> names = docs
+                .stream()
+                .map(UserControllerSpec::getName)
+                .sorted()
+                .collect(Collectors.toList());
+        List<String> expectedNames = Arrays.asList("Jamie", "Pat");
+        assertEquals("Names should match", expectedNames, names);
+    }
+
+    @Test
+    public void getSamById() {
+        String jsonResult = userController.getUser(samsIdString);
+        System.out.println(jsonResult);
+        Document sam = Document.parse(jsonResult);
+        System.out.println(sam);
+
+        assertEquals("Name should match", "Sam", sam.get("name"));
     }
 }
