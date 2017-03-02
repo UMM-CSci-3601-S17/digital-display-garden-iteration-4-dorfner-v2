e233528640fe415dace28d736f01af6b72f6efea
Joseph Thelen <thele116@morris.umn.edu>
2017-03-02 18:27:58 +0000

    Clean up gradle tasks
    Add descriptions and groups for our tasks
    Add clean task for client
    Add build task for client
    Change output dir for client build
    
    Issue: #4

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve('dist')
+        path: path.resolve('build')
     },
     plugins: [
         new ChunkWebpack({
