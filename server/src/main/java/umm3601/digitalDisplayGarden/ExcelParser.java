2f2ea8992df5ee4a6c048e0efff670456dff7f25
nathan-beneke <benek020@morris.umn.edu>
2017-03-04 18:35:49 -0600

    import Apache.poi

--- server/src/main/java/umm3601/digitalDisplayGarden/ExcelParser.java
+++ server/src/main/java/umm3601/digitalDisplayGarden/ExcelParser.java
@@ -1,15 +1,27 @@
 package umm3601.digitalDisplayGarden;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Iterator;
-import org.apache.poi.*;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 
 public class ExcelParser {
 
-    public static String returnsAnyThing() {
-        System.out.println("I return anything!");
-        return "I return anything!";
+    public static void returnsAnyThing() {
+        try {
+            FileInputStream file = new FileInputStream(new File("Accession list 2016.xls.xlsx"));
+
+            HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+            HSSFSheet sheet = workbook.getSheetAt(0);
+
+            System.out.println(sheet.getRow(1));
+        } catch (Exception E) {
+            System.out.println(E);
+        }
     }
 //    FileInputStream inp = new FileInputStream( file );
 //    Workbook workbook = WorkbookFactory.create( inp );
