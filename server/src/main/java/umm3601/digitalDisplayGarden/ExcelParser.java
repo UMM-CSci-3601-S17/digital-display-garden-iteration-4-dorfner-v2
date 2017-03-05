cd10fee9a48ee9da014b2dbef94471ac753fed7b
Dogxx000 <scot0530@morris.umn.edu>
2017-03-04 20:29:46 -0600

    Put xlsx data in 2D array, print 2D array WARNING: * Path is absolute to Lenny's account, need to fix * First row in 2d [0] is null, that's why the print function starts at 1. * Any cell with no value is null (mine field)

--- server/src/main/java/umm3601/digitalDisplayGarden/ExcelParser.java
+++ server/src/main/java/umm3601/digitalDisplayGarden/ExcelParser.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.Iterator;
 
 public class ExcelParser {
@@ -24,13 +25,17 @@ public class ExcelParser {
             Iterator<Row> iterator = datatypeSheet.iterator();
 
             // ---------- our stuff -----------
-            String[][] cellValues = new String[datatypeSheet.getLastRowNum()][];
+            String[][] cellValues = new String[datatypeSheet.getLastRowNum() + 1][];
+            Row currentRow = datatypeSheet.getRow(1);
             // ---------- our stuff -----------
 
-            while (iterator.hasNext()) {
+            Cell firstCell = iterator.next().getCell(1);
+            while (iterator.hasNext() && !firstCell.equals(null)) {
 
-                Row currentRow = iterator.next();
+                currentRow = iterator.next();
                 Iterator<Cell> cellIterator = currentRow.iterator();
+
+                cellValues[currentRow.getRowNum()] = new String[currentRow.getLastCellNum() - 1];
 
                 while (cellIterator.hasNext()) {
 
@@ -41,18 +46,37 @@ public class ExcelParser {
                     //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
                     if (currentCell.getCellTypeEnum() == CellType.STRING) {
                         System.out.print(currentCell.getStringCellValue() + "--");
+                        cellValues[currentCell.getRowIndex()][currentCell.getColumnIndex()] = currentCell.getStringCellValue();
                     } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
                         System.out.print(currentCell.getNumericCellValue() + "--");
+                        cellValues[currentCell.getRowIndex()][currentCell.getColumnIndex()] = ("" + currentCell.getNumericCellValue());
                     }
 
                 }
                 System.out.println();
 
+
             }
+
+            System.out.println(cellValues.length);
+            System.out.println(cellValues[1].length);
+            printDoubleArray(cellValues);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void printDoubleArray(String[][] input){
+        for(int i = 1; i < input.length; i++){
+            if (!input[i].equals(null)) {
+                for (int j = 0; j < input[i].length; j++) {
+                    System.out.print(" | " + input[i][j] );
+                }
+                System.out.println();
+                System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            }
         }
     }
 
