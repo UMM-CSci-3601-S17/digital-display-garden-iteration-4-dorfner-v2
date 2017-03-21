f10910c7b8665a2604fc7357881ed329f23109cd
frazi177 <frazi177@morris.umn.edu>
2017-03-21 13:57:03 -0500

    Change API_URL back to 'localhost' from 'bistromath'

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -52,7 +52,7 @@ module.exports = {
         }),
         new DefinePlugin({
             'ENV': JSON.stringify('development'),
-            'API_URL': JSON.stringify('http://bistromath.morris.umn.edu:2538/api/')
+            'API_URL': JSON.stringify('http://localhost:2538/api/')
         })
     ],
     resolve: {
