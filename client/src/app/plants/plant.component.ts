05f020a6896f1d61035df9bf805c46ecd0ca03b7
frazi177 <frazi177@morris.umn.edu>
2017-03-21 15:56:08 -0500

    Make dialog box use plant-component instead of hardcoding HTML
    Also changes how plantcomponent operates and is used in the HTML,
    you now specify which plant using [plant]="...".
    
    Issue #14 was caused by all of the dialog boxes sharing one boolean reference to
    act as their visibility signal. We fixed this by giving each plant page (PlantComponent)
    a showDialog boolean.
    
    Fixes #14

--- client/src/app/plants/plant.component.ts
+++ client/src/app/plants/plant.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+
 import { PlantListService } from "./plant-list.service";
 import { Plant } from "./plant";
+import { Component, OnInit, Input, Output, OnChanges, EventEmitter, trigger, state, style, animate, transition } from '@angular/core';
 
 @Component({
     selector: 'plant-component',
     templateUrl: 'plant.component.html'
 })
 export class PlantComponent implements OnInit {
-    public plant: Plant = null;
+    @Input() plant : Plant;
+    @Input() showDialog : boolean;
+    //public plant: Plant = null;
     private id: string;
 
     constructor(private plantListService: PlantListService) {
         // this.plants = this.plantListService.getPlants();
     }
 
+    /*
     private subscribeToServiceForId() {
         if (this.id) {
             this.plantListService.getPlantById(this.id).subscribe(
@@ -28,9 +32,10 @@ export class PlantComponent implements OnInit {
     setId(id: string) {
         this.id = id;
         this.subscribeToServiceForId();
-    }
+    }*/
 
     ngOnInit(): void {
-        this.subscribeToServiceForId();
+        //this.subscribeToServiceForId();
+        // console.log(this.plant.cultivar);
     }
 }
