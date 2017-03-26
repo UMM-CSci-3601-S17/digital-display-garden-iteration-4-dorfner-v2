2707ac8cc800b1b9ef7ad481af5b10096fa4c203
danfrz <frazi177@morris.umn.edu>
2017-03-26 07:53:26 -0500

    Try to implement CreateQRCodesFromAllBeds

--- server/src/main/java/umm3601/digitalDisplayGarden/QRCodes.java
+++ server/src/main/java/umm3601/digitalDisplayGarden/QRCodes.java
@@ -1,15 +1,14 @@
 package umm3601.digitalDisplayGarden;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.Buffer;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedOutputStream;
+import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipOutputStream;
 
 import javax.imageio.ImageIO;
 
@@ -44,8 +43,8 @@ public class QRCodes {
         this.plantController = plantController;
     }
 
-    public static void main(String[] args) throws WriterException, IOException,
-            NotFoundException {
+    public static void main(String[] args)
+            throws WriterException, IOException, NotFoundException {
         String qrCodeData = "Hello World!";
         String filePath = "QRCode.png";
         String charset = "UTF-8"; // or "ISO-8859-1"
@@ -56,46 +55,96 @@ public class QRCodes {
         //Files.write(filePath, img.getData());
         System.out.println("QR Code image created successfully!");
 
-        System.out.println("Data read from QR Code: "
-                + readQRCode(filePath, charset, hintMap));
-
     }
 
     public static BufferedImage createQRFromBedURL(String url) throws IOException,WriterException{
 
-        Map map = new HashMap();
-        map.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-        return createQRCode(url, "UTF-8", map, 300,300);
+        Map hintMap = new HashMap();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        return createQRCode(url, "UTF-8", hintMap, 300,300);
     }
 
 
-    public void CreateAllQRCodesfromAllBeds(){
+    public void CreateQRCodesFromAllBeds(){
         //Get all unique beds from Database
         //Create URLs for all unique beds
         //Create QRCode BufferedImages for all URLs
-        //Zip up all BufferedImages
+        //Zip up all BufferedImages (We have to tar/zip them up because we don't want to spam the WCROC admin with QRCode file for every bed)
+            //http://www.mkyong.com/java/how-to-convert-bufferedimage-to-byte-in-java/
+            //http://www.oracle.com/technetwork/articles/java/compress-1565076.html
         //create Post request to client to download zip.
 
-        String jsonBeds = plantController.getGardenLocations();
+        String bedNames[] = plantController.getGardenLocations();
+        final int numBeds = bedNames.length;
+
+        String bedURLs[] = new String [numBeds];
+        List<BufferedImage> qrCodeImages = new ArrayList<BufferedImage>();
+
+        for(int i = 0; i < numBeds; i++) {
+            bedURLs[i] = "http://localhost:9000/bed/" + bedNames[i];
+            try {
+                qrCodeImages.add(createQRFromBedURL(bedURLs[i]));
+            }
+            catch(IOException ioe)
+            {
+                //It would be really bad for a qrCodeImage to crash out and be null.
+                //TODO: We should handle this better, what should we do?
+                ioe.printStackTrace();
+            }
+            catch(WriterException we)
+            {
+                we.printStackTrace();
+            }
+        }
+
+        //Archive and Compress into Zip
+
+        final int BUFFER_SIZE = 1024;
+
+        //FileOutputStream dest = new FileOutputStream("somefile"); //TODO: should not go to file
+        CheckedOutputStream checksum = new CheckedOutputStream(null, new Adler32()); //null should be replaced with an OutputStream
+        ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(checksum));
+
+        //out.setMethod(ZipOutputStream.DEFLATED);
+
+        byte dataBuffer[] = new byte[BUFFER_SIZE];
+
+        //Get bytes from images and write them into the zipOutputStream
+        byte imageData[];
+        for(int i = 0; i < qrCodeImages.size(); i++)
+        {
+            ByteArrayOutputStream toByteStream = new ByteArrayOutputStream();
+            try {
+                ImageIO.write(qrCodeImages.get(i), "png", toByteStream); //formatName was jpg -- does changing it to png just _rename_ it a png or actually make it png?
+                toByteStream.flush();
+                imageData = toByteStream.toByteArray();
+                //imageData contains the bytes for the qr images, now we just need to put it in the ZipOutputStream
+                //and then send the zip stream to the client in a way it will recognize
+
+
+
+            }
+            catch(IOException ioe)
+            {
+                //TODO: handle better
+                ioe.printStackTrace();
+            }
+
+
+        }
 
 
     }
 
     public static BufferedImage createQRCode(String qrCodeData, String charset, Map hintMap, int qrCodeheight, int qrCodewidth)
             throws WriterException, IOException {
+        //Create the BitMatrix representing the QR code
         BitMatrix matrix = new MultiFormatWriter().encode(
                 new String(qrCodeData.getBytes(charset), charset),
                 BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
-        return MatrixToImageWriter.toBufferedImage(matrix);
-    }
 
-    public static String readQRCode(String filePath, String charset, Map hintMap)
-            throws FileNotFoundException, IOException, NotFoundException {
-        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
-                new BufferedImageLuminanceSource(
-                        ImageIO.read(new FileInputStream(filePath)))));
-        Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap,hintMap);
-        return qrCodeResult.getText();
+        //Create BufferedImages from the QRCode BitMatricies
+        return MatrixToImageWriter.toBufferedImage(matrix);
     }
 
 }
