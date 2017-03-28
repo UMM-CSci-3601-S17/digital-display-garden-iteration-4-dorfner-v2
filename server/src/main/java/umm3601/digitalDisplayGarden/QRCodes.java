6a12a929bae521a8049aeb8fbbf2cfcf475ce79e
Shamund Gordon <stgordon19@gmail.com>
2017-03-28 12:50:32 -0500

    Remove unused imports

--- server/src/main/java/umm3601/digitalDisplayGarden/QRCodes.java
+++ server/src/main/java/umm3601/digitalDisplayGarden/QRCodes.java
@@ -1,9 +1,7 @@
 package umm3601.digitalDisplayGarden;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -13,17 +11,11 @@ import java.util.zip.*;
 import javax.imageio.ImageIO;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.BinaryBitmap;
 import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatReader;
 import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.Result;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 
