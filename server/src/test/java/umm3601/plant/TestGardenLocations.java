38015232f4138eef3c8d8ead5fa1fda56fb51193
Tony Song <songx823@morris.umn.edu>
2017-04-05 16:43:25 -0500

    Add tests for 'GetGardenLocations' in 'PlantController.java'

--- server/src/test/java/umm3601/plant/TestGardenLocations.java
+++ server/src/test/java/umm3601/plant/TestGardenLocations.java
@@ -3,6 +3,7 @@ package umm3601.plant;
 import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
 import umm3601.digitalDisplayGarden.PlantController;
 
 import java.io.IOException;
@@ -25,12 +26,25 @@ public class TestGardenLocations {
     public void GetGardenLocationAsJsonRemovesDuplicates() throws IOException {
         GardenLocation[] gardenLocations;
         Gson gson = new Gson();
+        GardenLocation dummy = new GardenLocation();
+        dummy._id = "10.0";
 
         String json = plantController.getGardenLocationsAsJson("first uploadId");
         gardenLocations = gson.fromJson(json, GardenLocation[].class);
         assertEquals("Incorrect number of unique garden locations", 1, gardenLocations.length);
-        assertEquals("Incorrect zero index", "10.0", gardenLocations[0]._id);
+        assertEquals("Incorrect zero index", dummy, gardenLocations[0]);
         assertEquals("Incorrect value for index 0", "10.0", gardenLocations[0]._id);
     }
 
+    @Test
+    public void GetGardenLocationsRemovesDuplicates() throws IOException {
+        String[] gardenLocations;
+        Gson gson = new Gson();
+        String[] dummy = {"10.0"};
+
+        gardenLocations = plantController.getGardenLocations("first uploadId");
+        assertEquals("Incorrect number of unique garden locations", 1, gardenLocations.length);
+        assertArrayEquals("Incorrect zero index", dummy, gardenLocations);
+        assertEquals("Incorrect value for index 0", "10.0", gardenLocations[0]);
+    }
 }
