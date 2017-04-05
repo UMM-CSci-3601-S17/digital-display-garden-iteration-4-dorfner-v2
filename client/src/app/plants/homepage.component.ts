4a0e99bf24c9dd3801c5a14194f97547f02af57b
hoff0899 <hoff0899@morris.umn.edu>
2017-04-05 14:50:21 -0500

    Create homepage.component.html and hompepage.component.ts
    Issue: #12

---
+++ client/src/app/plants/homepage.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { PlantListService } from "./plant-list.service";
+import { Plant } from "./plant";
+import {Params, ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+    selector: 'homepage-component',
+    templateUrl: 'homepage-component.html',
+})
+
+export class HomepageComponent implements OnInit {
+    public bed : string;
+    public plants: Plant[];
+    public locations: Plant[];
+
+    constructor(private plantListService: PlantListService, private route: ActivatedRoute, private router: Router) {
+        // this.plants = this.plantListService.getPlants()
+
+        //Get the bed from the params of the route
+        this.router.events.subscribe((val) => {
+            this.bed = this.route.snapshot.params["gardenLocation"];
+            this.refreshInformation();
+        });
+    }
+
+
+    ngOnInit(): void{
+    }
+
+    refreshInformation() : void
+    {
+        this.plantListService.getFlowersByBed(this.bed).subscribe (
+            plants => this.plants = plants,
+            err => {
+                console.log(err);
+            }
+        );
+
+        this.plantListService.getGardenLocations().subscribe(
+            locations => this.locations = locations,
+            err => {
+                console.log(err);
+            }
+        );
+    }
+
+
+}
