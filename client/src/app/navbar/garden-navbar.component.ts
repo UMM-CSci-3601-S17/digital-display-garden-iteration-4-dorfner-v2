1ff1dc24cdb6095affdc6256d515498001f66183
Tony Song <songx823@morris.umn.edu>
2017-04-09 17:44:18 -0500

    Move the bed dropdown to the navigation component
    Issue: #9

--- client/src/app/navbar/garden-navbar.component.ts
+++ client/src/app/navbar/garden-navbar.component.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlantListService } from "../plants/plant-list.service";
+import { Plant } from "../plants/plant";
+import {Params, ActivatedRoute, Router} from "@angular/router";
+
 
 @Component({
     selector: 'garden-navbar-component',
     templateUrl: 'garden-navbar.component.html'
 })
 
-export class GardenNavbarComponent {}
+export class GardenNavbarComponent {
+    public bed : string;
+    public plants: Plant[];
+    public locations: Plant[];
+
+    constructor(private plantListService: PlantListService, private route: ActivatedRoute, private router: Router) {
+        //Get the bed from the params of the route
+
+    }
+
+    ngOnInit(): void{
+        this.router.events.subscribe((val) => {
+            this.bed = this.route.snapshot.params["gardenLocation"];
+            this.refreshInformation();
+        });
+    }
+
+    refreshInformation() : void
+    {
+        this.plantListService.getFlowersByBed(this.bed).subscribe (
+            plants => this.plants = plants,
+            err => {
+                console.log(err);
+            }
+        );
+
+        this.plantListService.getGardenLocations().subscribe(
+            locations => this.locations = locations,
+            err => {
+                console.log(err);
+            }
+        );
+    }
+}
