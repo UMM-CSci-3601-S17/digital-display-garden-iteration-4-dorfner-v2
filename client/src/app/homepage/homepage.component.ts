da76d5aa44067e9770bb10ad29d9194f415bac09
Tony Song <songx823@morris.umn.edu>
2017-04-09 16:39:33 -0500

    Add a feature to search plants by common names
    Issue: #9

--- client/src/app/homepage/homepage.component.ts
+++ client/src/app/homepage/homepage.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { PlantListService } from "../plants/plant-list.service";
 import { Plant } from "../plants/plant";
 import {Params, ActivatedRoute, Router} from "@angular/router";
+import { FilterBy } from "../plants/filter.pipe";
 
 @Component({
     selector: 'homepage-component',
     templateUrl: 'homepage.component.html',
+    providers: [ FilterBy ]
 })
 
 export class HomepageComponent implements OnInit {
     public bed : string;
-    public plants: Plant[];
+    public plants: Plant[]; // not used - so delete it
     public locations: Plant[];
+    public currentCN: string;
+    public currentList: Plant[];
 
     constructor(private plantListService: PlantListService, private router: Router) {
         this.refreshInformation();
@@ -19,8 +23,16 @@ export class HomepageComponent implements OnInit {
 
 
     ngOnInit(): void{
+        this.plantListService.getPlants().subscribe(
+            currentList => this.currentList = currentList,
+            err => {
+                console.log(err);
+            }
+        );
     }
 
+    // Get rid of this method and move `this.plantListService.getGardenLocations().subscribe(`
+    // to the constructor. Also, get rid of `plants[]`
     refreshInformation() : void
     {
         this.plantListService.getFlowersByBed(this.bed).subscribe (
@@ -37,6 +49,4 @@ export class HomepageComponent implements OnInit {
             }
         );
     }
-
-
 }
