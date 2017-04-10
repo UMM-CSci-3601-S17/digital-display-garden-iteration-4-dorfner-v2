3d7c0ccdd75e261431196142a68877a11c9ec61a
Laverne Schrock <schr1230@morris.umn.edu>
2017-04-10 14:06:27 -0500

    Creating testing framework for DeleteComponent
    Issue: #7

---
+++ client/src/app/admin/delete.component.spec.ts
@@ -0,0 +1,57 @@
+import {ComponentFixture, TestBed, async} from "@angular/core/testing";
+import { Observable } from "rxjs";
+import {FormsModule} from "@angular/forms";
+import {DeleteComponent} from "./delete.component";
+import {AdminService} from "./admin.service";
+import {RouterTestingModule} from "@angular/router/testing";
+import {NavbarComponent} from "../navbar/navbar.component";
+
+describe("Delete Component", () => {
+
+    let deleteComponent: DeleteComponent;
+    let fixture: ComponentFixture<DeleteComponent>;
+    let adminServiceStub: {
+        getUploadIds: () => Observable<string[]>,
+        getLiveUploadId: () => Observable<string>,
+        deleteUploadId: (string) => Observable<any>,
+    };
+
+    beforeEach(() => {
+        adminServiceStub = {
+            getUploadIds: () => {
+                return Observable.of(["upload id 1", "upload id 2"]);
+            },
+            getLiveUploadId: () => {
+                return Observable.of("upload id 2");
+            },
+            deleteUploadId: (uploadID: string) => {
+
+                return Observable.of({
+                    success: ["upload id 1", "upload id 2"].filter(str => str !== uploadID).length === 1,
+                    uploadIds: ["upload id 1", "upload id 2"].filter(str => str !== uploadID)
+                });
+            }
+        };
+
+        TestBed.configureTestingModule({
+            imports: [FormsModule, RouterTestingModule ],
+            declarations: [ DeleteComponent, NavbarComponent],
+            providers:    [{provide: AdminService, useValue: adminServiceStub}]
+        });
+
+    });
+
+    beforeEach(
+        async(() => {
+            TestBed.compileComponents().then(() => {
+                fixture = TestBed.createComponent(DeleteComponent);
+                deleteComponent = fixture.componentInstance;
+                fixture.detectChanges();
+            });
+    }));
+
+    it("can be initialized", () => {
+        expect(deleteComponent).toBeDefined();
+    });
+
+});
